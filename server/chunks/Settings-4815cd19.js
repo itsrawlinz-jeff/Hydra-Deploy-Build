d290253712c2fb0fbc8a261953f8d55cb2b6e545
Jeffrey Rowlings <rawlinzjeff264@gmail.com>
2023-11-24 17:18:04 +0300

    File Updates

--- server/chunks/Settings-4815cd19.js
+++
@@ -1,9 +0,0 @@
-import { d as defaultModel } from './models-5d0b4e4a.js';
-
-const DEFAULT_SETTINGS = {
-  shareConversationsWithModelAuthors: true,
-  activeModel: defaultModel.id
-};
-
-export { DEFAULT_SETTINGS as D };
-//# sourceMappingURL=Settings-4815cd19.js.map
--- server/chunks/titleUpdate-ae4a3ca1.js
+++
@@ -1,6 +0,0 @@
-import { w as writable } from './index2-05938230.js';
-
-const titleUpdate = writable(null);
-
-export { titleUpdate as t };
-//# sourceMappingURL=titleUpdate-ae4a3ca1.js.map
